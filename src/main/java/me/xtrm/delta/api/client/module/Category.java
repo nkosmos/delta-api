3af406080cec7404003b4ac39440b587a427c389
xTrM_ <xtrmyt24@gmail.com>
2021-08-04 04:41:40 +0200

    catege

--- src/main/java/me/xtrm/delta/api/client/module/Category.java
+++ src/main/java/me/xtrm/delta/api/client/module/Category.java
@@ -1,12 +1,15 @@
 package me.xtrm.delta.api.client.module;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 
-@AllArgsConstructor
-@RequiredArgsConstructor
-public @Data class Category {
+import java.util.ArrayList;
+import java.util.List;
+
+public @Data
+class Category {
+
+    private static List<Category> REGISTRY = new ArrayList<>();
+
     public static final Category COMBAT = new Category("Combat", '4');
     public static final Category MOVEMENT = new Category("Movement", 's');
     public static final Category PLAYER = new Category("Player", 'u');
@@ -17,5 +20,25 @@ public @Data class Category {
 
     private final String name;
     private final char iconChar;
-    private boolean visible = true;
+    private boolean visible;
+
+    public Category(String name, char iconChar, boolean visible) {
+        this.name = name;
+        this.iconChar = iconChar;
+        this.visible = visible;
+
+        if (REGISTRY.stream().anyMatch(c -> c.name.equalsIgnoreCase(name))) {
+            throw new RuntimeException("Cannot add already existing Category named: " + name);
+        }
+
+        REGISTRY.add(this);
+    }
+
+    public Category(String name, char iconChar) {
+        this(name, iconChar, true);
+    }
+
+    public static Category fromName(String name) {
+        return REGISTRY.stream().filter(category -> category.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
+    }
 }
